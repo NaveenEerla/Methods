4868b8ea9164803d029af6143b2074cef1a1e1ef
navee <navee@DESKTOP-IKVTSIS>
2024-07-24 14:11:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeetAlien();
+        }
+       static void MeetAlien()
+        {
+            Random numGen= new Random();
+            string name = "z-" + numGen.Next(10, 1000);
+            int age=numGen.Next(10, 500);
+            Console.WriteLine("Hi, I'm "+ name);
+            Console.WriteLine("i'm "+ age+ " Years Old !");
+            Console.WriteLine("and I'm an alien..<>");
+        }
+    }
+}
